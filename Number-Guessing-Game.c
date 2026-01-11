53280fe9e0995f27d650eb348961882d5394080f
Nikhil-Kumawat369 <coder6185@gmail.com>
2026-01-11 12:55:19 +0530

    Added single player Custom MODE

--- Number-Guessing-Game.c
+++ Number-Guessing-Game.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <time.h>
 
-void setupGame (int *ptr , int *ptrStart , int *ptrEnd , int *ptrMultiplayerRangeOPT , int *ptrplayerNumber) ; // this sets up the full game
+void setupGame (int *ptr , int *ptrStart , int *ptrEnd) ; // this sets up the full game
 
 void randomNumber (int *targetNumber , int rangeStart , int rangeEnd) ; // it decides the target number
 
-int hint (int *guessNumber , int *targetNumber , int *c) ; // this give hint & warning msg
+int hint (int *guessNumber , int *targetNumber , int *c , int rangeStart , int rangeEnd) ; // this give hint & warning msg
 
 int main () {
 
@@ -26,7 +26,7 @@ int main () {
 
     srand(time(0)) ; // setting the seed for selecting random number 
 
-    setupGame (&mode , &rangeStart , &rangeEnd , &multiplayerRangeOPT , &playerNumber) ;
+    setupGame (&mode , &rangeStart , &rangeEnd) ;
 
     if (mode == 0) { // this is Classic Mode (Single Player) code ...
 
@@ -76,7 +76,7 @@ int main () {
             printf(">> ");
             scanf("%d" , &guessNumber) ;
 
-            x = hint (&guessNumber , &targetNumber , &c) ;
+            x = hint (&guessNumber , &targetNumber , &c , 1 , 100) ;
 
             if (x == 1) {
                 continue ; // if user enters invalid then he got the penelty so ne future exesution in this itteration
@@ -126,11 +126,69 @@ int main () {
 
     }
 
+    if (mode == 1) { // this is Custom Mode (Single Player) code ...
+
+        char playerName1[50] ;
+
+        printf("\n========================================\n");
+        printf("        SINGLE PLAYER - CUSTOM MODE\n");
+        printf("========================================\n\n");
+
+        printf("The computer has selected a random number\n");
+        printf("between %d and %d.\n\n" , rangeStart , rangeEnd);
+
+        printf("Your goal is simple:\n");
+        printf("=> Guess the correct number\n");
+        printf("=> Get hints after every wrong attempt\n");
+        printf("=> Solve it in the LEAST number of attempts\n\n");
+
+        printf("Fewer attempts means a higher score!\n");
+        printf("Your best score will not be saved.\n\n");
+
+        printf("Good luck - play smart!\n");
+
+        printf("\n----------------------------------------\n");
+        printf("PLAYER SETUP\n");
+        printf("----------------------------------------\n\n");
+
+        printf("Enter your player name to begin:\n");
+        printf(">> ");
+        getchar() ; // To clear the buffer
+        fgets(playerName1 , 50 , stdin) ;
+
+        playerName1[strcspn(playerName1 , "\n")] = '\0' ; // fget c always saved \n{Enter} too , it replaces it with \0
+
+        randomNumber (&targetNumber , rangeStart , rangeEnd) ; // this would pick a random number between the range
+
+        int guessNumber ; // this saves the users guess
+        int c = 1 ; // Number of attempts
+        int x ; // this a variable which will store values temperorly in the code , when ever needed
+
+        do { // This takes input , checks if its correct guess & tracks the no of attemps too
+
+            printf("Enter Your Guess %d\n" , c) ;
+            printf(">> ");
+            scanf("%d" , &guessNumber) ;
+
+            x = hint (&guessNumber , &targetNumber , &c , rangeStart , rangeEnd) ;
+
+            if (x == 1) {
+                continue ; // if user enters invalid then he got the penelty so ne future exesution in this itteration
+            }else if (x == 100) {
+                break ; // if user enters correct the the loop needs to end
+            }
+
+        }while (guessNumber != targetNumber) ;
+
+        printf("\n\n##-- %s You guessed in %d attemps --##\n\n" , playerName1 , c) ;
+
+    }
+
     return 0 ;
 
 }
 
-void setupGame (int *ptr , int *ptrStart , int *ptrEnd , int *ptrMultiplayerRangeOPT , int *ptrplayerNumber) {
+void setupGame (int *ptr , int *ptrStart , int *ptrEnd) {
 
     printf("========================================\n");
     printf("      WELCOME TO THE NUMBER GAME\n");
@@ -140,8 +198,7 @@ void setupGame (int *ptr , int *ptrStart , int *ptrEnd , int *ptrMultiplayerRang
 
     printf("[ 0 ] Classic Mode  (Single Player)\n");
     printf("[ 1 ] Custom Mode   (Single Player)\n");
-    printf("[ 2 ] Multiplayer Mode\n");
-    printf("[ 3 ] Exit Game\n\n");
+    printf("[ 2 ] Exit Game\n\n");
 
     printf("----------------------------------------\n");
     printf("Enter your choice: ");
@@ -178,112 +235,13 @@ void setupGame (int *ptr , int *ptrStart , int *ptrEnd , int *ptrMultiplayerRang
 
                 break ;
 
-        case 2 :printf("\n----------------------------------------\n");
-                printf("MULTIPLAYER MODE\n");
-                printf("----------------------------------------\n\n");
-
-                printf("Play with friends and take turns!\n");
-                printf("No High Scores are saved in this mode.\n\n");
-
-                printf("Choose number range:\n\n");
-                printf("[ 0 ] Standard Range (1 to 100)\n");
-                printf("[ 1 ] Custom Range\n\n");
-
-                printf("----------------------------------------\n");
-                printf("Enter your choice: ");
-                scanf("%d" , ptrMultiplayerRangeOPT) ;
-
-                if (*ptrMultiplayerRangeOPT == 0) {
-
-                    printf("\n----------------------------------------\n");
-                    printf("PLAYER SETUP\n");
-                    printf("----------------------------------------\n\n");
-
-                    printf("Enter number of players (minimum 2): ");
-                    scanf("%d" , ptrplayerNumber) ;
-
-                    if (*ptrplayerNumber < 2) {
-
-                        printf("\n----------------------------------------\n");
-                        printf("OOPS! THAT DIDN'T WORK\n");
-                        printf("----------------------------------------\n\n");
-
-                        printf("Multiplayer mode needs at least 2 players.\n");
-                        printf("Playing alone is not multiplayer!\n\n");
-
-                        printf("Please restart the game and try again.\n");
-                        printf("Exiting now...\n");
-
-                        exit(1) ;
-
-                    }
-
-                }else if (*ptrMultiplayerRangeOPT == 1) {
-
-                    printf("\n----------------------------------------\n");
-                    printf("CUSTOM MODE - MULTI PLAYER\n");
-                    printf("----------------------------------------\n\n");
-
-                    printf("=> You choose your own number range\n");
-                    printf("=> Computer selects a random number\n");
-                    printf("=> MULTIPLE player can play\n");
-                    printf("=> High Score will NOT be saved\n\n");
-
-                    printf("Enter your custom range to continue...\n");
-                    printf("Range start from :- ") ;
-                    scanf("%d" , ptrStart) ;
-                    printf("Range till :- ") ;
-                    scanf("%d" , ptrEnd) ;
-                    printf("\n") ;
-
-                    printf("\n----------------------------------------\n");
-                    printf("PLAYER SETUP\n");
-                    printf("----------------------------------------\n\n");
-
-                    printf("Enter number of players (minimum 2): ");
-                    scanf("%d" , ptrplayerNumber) ;
-
-                    if (*ptrplayerNumber < 2) {
-
-                        printf("\n----------------------------------------\n");
-                        printf("OOPS! THAT DIDN'T WORK\n");
-                        printf("----------------------------------------\n\n");
-
-                        printf("Multiplayer mode needs at least 2 players.\n");
-                        printf("Playing alone is not multiplayer!\n\n");
-
-                        printf("Please restart the game and try again.\n");
-                        printf("Exiting now...\n");
-
-                        exit(1) ;
-
-                    }
-
-                }else {
-
-                    printf("\n----------------------------------------\n");
-                    printf("WRONG MOVE!\n");
-                    printf("----------------------------------------\n\n");
-
-                    printf("That option does not exist.\n");
-                    printf("Rules were broken or input was invalid.\n\n");
-
-                    printf("Restart the game to try again.\n");
-                    printf("Exiting...\n");
-
-                    exit(1) ;
-
-                }
-
-                break ;
-
         default :printf("\n----------------------------------------\n");
                 printf("EXITING GAME\n");
                 printf("----------------------------------------\n\n");
 
                 printf("Thanks for playing!\n");
                 printf("Hope to see you again soon.\n");
-                printf("Goodbye 👋\n");
+                printf("Goodbye \n");
 
                 exit(0) ;
 
@@ -301,9 +259,9 @@ void randomNumber (int *targetNumber , int rangeStart , int rangeEnd) {
 
 }
 
-int hint (int *guessNumber , int *targetNumber , int *c) {
+int hint (int *guessNumber , int *targetNumber , int *c , int rangeStart , int rangeEnd) {
 
-    if (*guessNumber < 0 || *guessNumber > 100) {
+    if (*guessNumber < rangeStart || *guessNumber > rangeEnd) {
 
         printf("\nInvalid move!\n");
         printf("The number you entered is outside the allowed range.\n");
